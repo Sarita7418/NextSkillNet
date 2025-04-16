7e13f0de948d3a004c2c0ddfbd691149c76524d5
Sarita731 <mg6667418@gmail.com>
2025-04-16 19:24:05 -0400

    first-conf

---
+++ frontend/app/Institucional/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Institucional() {
+  return (
+    <div>Institucional</div>
+  )
+}
+
+export default Institucional
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -23,7 +23,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto:ital,wdth,wght@0,75..100,100..900;1,75..100,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
